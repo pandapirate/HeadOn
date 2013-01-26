09b74a09e29608c7b1470917e2f685739ebad895
Kevin <kevinchh88@gmail.com>
2013-01-26 11:37:01 -0800

    Initial Commit

---
+++ HeadOn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HeadOn
+//
+//  Created by Kevin Huang on 1/26/13.
+//  Copyright The Company 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
